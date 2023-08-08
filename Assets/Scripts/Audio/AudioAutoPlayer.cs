94376a40f9911c96207e379c0e832dc9ab75693d
Emre Dogan <48212096+EmreDogann@users.noreply.github.com>
2023-08-08 05:06:30 +0100

    Fixed more tutorial bugs.

--- Assets/Scripts/Audio/AudioAutoPlayer.cs
+++ Assets/Scripts/Audio/AudioAutoPlayer.cs
@@ -7,11 +7,17 @@ namespace Audio
     {
         [SerializeField] private AudioSO audioToPlay;
         [SerializeField] private GameObject attachToGameObject;
+        [SerializeField] private bool stopExistingAudio;
         [SerializeField] private bool fadeIn;
         [ConditionalField(nameof(fadeIn))] [SerializeField] private float fadeInDuration = 1.0f;
 
         private void Start()
         {
+            if (stopExistingAudio)
+            {
+                audioToPlay.StopAll();
+            }
+
             if (attachToGameObject)
             {
                 audioToPlay.PlayAttached(attachToGameObject);
